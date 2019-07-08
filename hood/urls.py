b1d74df22ddd04623a4a34992b9df8699aea9774
Christine <you@example.com>
2019-07-08 13:03:50 +0300

    adds leave url confg

--- hood/urls.py
+++ hood/urls.py
@@ -13,8 +13,8 @@ urlpatterns=[
     url(r'^business/(\d+)$', views.business, name='business'),
     url(r'^add/business/(\d+)$', views.add_business, name='add-business'),
     url(r'^post/(\d+)$', views.post, name='post'),
-    
-    
+    url(r'^search/',views.search_results, name='search_results'),
+    url(r'^leave/$', views.leave, name='leave'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
